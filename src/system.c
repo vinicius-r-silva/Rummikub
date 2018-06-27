ae37be9b48666b85fab6d58853d99d1b17dc02bd
7Nic <39524325+7Nic@users.noreply.github.com>
2018-06-27 00:54:11 -0300

    Introdução duplicar mesa debugado

--- src/system.c
+++ src/system.c
@@ -1,5 +1,177 @@
+#define _CRT_SECURE_NO_DEPRECATE
 #include "libs/system.h"
 
+
+void Organiza_Mesa(LISTA_MESA_PTR *Mesa){
+    if(*Mesa == NULL)
+        return;
+
+    int linha = 0;
+    int coluna = 0;
+
+    LISTA_MESA_PTR mesa_atual = *Mesa;
+
+    while(mesa_atual != NULL){
+        if (coluna + mesa_atual->N_Cartas > N_MAX_COLUNAS && coluna != 0){
+            coluna = 0;
+            linha++;
+        }
+
+        mesa_atual->x = coluna;
+        mesa_atual->y = linha;
+
+        coluna += mesa_atual->N_Cartas + 1;
+        mesa_atual = mesa_atual->prox;
+    }
+}
+
+
+//verifica a validade da mesa (1-valido/0-invalido/-1 erro)
+int verifica_mesa(LISTA_MESA_PTR *lista_mesa){
+    LISTA_MESA_PTR atual_mesa = *lista_mesa;
+    //percorre os ponteiros da mesa atÃ© o ultimo
+    while(atual_mesa != NULL){
+        int qtd = 0;
+        LISTA_CARTAS_PTR monte1 = atual_mesa->cartas;
+        while(monte1 != NULL){
+            qtd++;
+            monte1 = monte1->prox;
+        }
+        //se tiver menos de 3 cartas Ã© invÃ¡lido
+        if (qtd < 3) return 0;
+
+        monte1 = atual_mesa->cartas;   //reseta para a primera posiÃ§Ã£o do monte
+        LISTA_CARTAS_PTR monte2 = atual_mesa->cartas;
+        monte2 = monte2->prox;
+
+        //identifica o tipo de monte
+        //tipo 0: invÃ¡lido
+        //tipo 1: sequencia de naipe igual
+        //tipo 2: grupo (cartas de numeros iguais de naipes diferentes)
+
+        //verificaÃ§Ã£o de uso do naipe, utilizado no case 2
+        int usado[4]; //[1, 2, 3, 4] 1-true, 0-false
+
+        //seta todos para false
+        for (int i=0; i<4; i++){
+            usado[i] = 0;
+        }
+
+
+        int tipo = 0;
+        if (monte1->naipe == monte2->naipe && monte1->numero == ((monte2->numero) -1)) tipo = 1;
+        if (monte1->naipe != monte2->naipe && monte1->numero == monte2->numero) tipo = 2;
+        if (tipo == 0) return 0;
+        //marca como usados os naipes
+        usado[(monte1->naipe) -1] = 1;
+        usado[(monte2->naipe) -1] = 1;
+
+
+        //anda uma posiÃ§Ã£o em cada
+        monte1 = monte1->prox;
+        monte2 = monte2->prox;
+
+
+        switch (tipo){
+        case 1:
+            while (monte2 != NULL){
+                if (monte1->naipe != monte2->naipe || monte1->numero != ((monte2->numero) -1)) return 0;
+                monte1 = monte1->prox;
+                monte2 = monte2->prox;
+            }
+        break;
+
+        case 2:
+            while (monte2 != NULL){
+                if (usado[(monte2->naipe) -1] == 1) return 0;
+                usado[(monte1->naipe) -1] = 1;
+                usado[(monte2->naipe) -1] = 1;
+                if (monte1->naipe == monte2->naipe || monte1->numero != monte2->numero) return 0;
+                monte1 = monte1->prox;
+                monte2 = monte2->prox;
+            }
+        break;
+
+        default:
+            return -1;
+        }
+        atual_mesa = atual_mesa->prox;
+    }
+    return 1;
+}
+
+void Baralho_2_mao(LISTA_CARTAS_PTR *baralho, LISTA_CARTAS_PTR *mao){
+    LISTA_CARTAS_PTR Carta = *baralho;
+    if (Carta == NULL)
+        return;
+
+    *baralho = Carta->prox;
+    Carta->prox = *mao;
+    *mao = Carta;
+}
+
+void mao_2_monte(LISTA_CARTAS_PTR *mao, LISTA_MESA_PTR *mesa, int Naipe, int Numero, int Pos, bool Nova_Lista){
+    LISTA_CARTAS_PTR prev_mao = NULL;
+    LISTA_CARTAS_PTR atual_mao = *mao;
+    while(atual_mao != NULL && atual_mao->naipe != Naipe && atual_mao->numero != Numero){
+        prev_mao = atual_mao;
+        atual_mao = atual_mao->prox;
+    }
+    if(atual_mao == NULL){
+        printf("Erro 10 - Carta Nao encontrada\n");
+        return;
+    }
+
+    if(prev_mao == NULL)
+        *mao = atual_mao->prox;
+    else
+        prev_mao->prox = atual_mao->prox;
+
+    atual_mao->prox = NULL;
+    LISTA_MESA_PTR Mesa_Atual;
+
+    if(Nova_Lista){
+        LISTA_MESA_PTR prev_mesa = NULL;
+        LISTA_MESA_PTR atual_mesa = *mesa;
+
+        LISTA_MESA_PTR Nova_mesa = (LISTA_MESA_PTR)malloc(sizeof(LISTA_MESA));
+        Nova_mesa->x = 0;
+        Nova_mesa->y = 0;
+        Nova_mesa->N_Cartas = 0;
+        Nova_mesa->prox = NULL;
+        Nova_mesa->cartas = NULL;
+
+        while(atual_mesa != NULL){
+            prev_mesa = atual_mesa;
+            atual_mesa = atual_mesa->prox;
+        }
+        if(prev_mesa == NULL)
+            *mesa = Nova_mesa;
+        else
+            prev_mesa->prox = Nova_mesa;
+
+        Pos = 0;
+        Mesa_Atual = Nova_mesa;
+    }
+    else
+        Mesa_Atual = *mesa;
+
+    int cont = 1;
+    LISTA_CARTAS_PTR prev_destino = NULL;
+    LISTA_CARTAS_PTR atual_destino = Mesa_Atual->cartas;
+
+    while(atual_destino != NULL && cont < Pos){
+        prev_destino = atual_destino;
+        atual_destino = atual_destino->prox;
+    }
+    if(prev_destino == NULL)
+        Mesa_Atual->cartas = atual_mao;
+    else
+        prev_destino->prox = atual_mao;
+
+    Mesa_Atual->N_Cartas++;
+    atual_mao->prox = atual_destino;
+}
 
 void Deleta_Lista(LISTA_CARTAS_PTR *Lista){
     LISTA_CARTAS_PTR atual = *Lista;
@@ -42,7 +214,7 @@ void TrocaCarta(LISTA_CARTAS_PTR *Baralho, int Naipe_1, int Naipe_2, int Num_1, 
         Carta1->numero = Num_2;
         Carta1->naipe = Naipe_2;
     } else
-        printf("Erro: Carta 1 não encontrada\n");
+        printf("Erro: Carta 1 não  encontrada\n");
 
     if(Carta2 != NULL){
         Carta2->numero = Num_1;
@@ -76,13 +248,153 @@ void Cria_Baralho(LISTA_CARTAS_PTR *Baralho){
         Cria_Baralho(Baralho);
 }
 
+int Hexa_2_int(char Numero){
+    if (Numero >= '0' && Numero <= '9')
+        Numero -= '0';
+    else if (Numero >= 'A' && Numero <= 'Z')
+        Numero -= 'A' - 10;
+    else if (Numero >= 'a' && Numero <= 'z')
+        Numero -= 'a' - 10;
+    else if (Numero == '*')
+        Numero = -1;
 
-//-----------------------FUNÇÕES INUTEIS---------------------------------------------------------------//
-void Imprime(LISTA_CARTAS_PTR Baralho){
+    return (int)Numero;
+}
+
+char int_2_hexa(int Numero){
+    if (Numero >= 0 && Numero <= 9)
+        Numero += '0';
+    else
+        Numero += 'A';
+
+    return (char)Numero;
+}
+
+char Int_2_Naipe(int Naipe){
+    switch(Naipe){
+        case (1):
+            return NAIPE_1;
+
+        case (2):
+            return NAIPE_2;
+
+        case (3):
+            return NAIPE_3;
+
+        case (4):
+            return NAIPE_4;
+
+        case (-1):
+            return CORINGA;
+
+        default:
+            return -1;
+    }
+}
+
+int Naipe_2_int(char Naipe){
+    switch(Naipe){
+        case (NAIPE_1):
+            return 1;
+
+        case (NAIPE_2):
+            return 2;
+
+        case (NAIPE_3):
+            return 3;
+
+        case (NAIPE_4):
+            return 4;
+
+        case (CORINGA):
+            return -1;
+
+        default:
+            return -1;
+    }
+}
+
+void Inverte_Lista(LISTA_CARTAS_PTR *Lista){
+    LISTA_CARTAS_PTR atual = *Lista;
+    LISTA_CARTAS_PTR prev = NULL;
+    LISTA_CARTAS_PTR pos = NULL;
+
+    if(atual == NULL)
+        return;
+
+    prev = atual;
+    atual = atual->prox;
+    pos = atual->prox;
+    prev->prox = NULL;
+    while(pos != NULL){
+        atual->prox = prev;
+        prev = atual;
+        atual = pos;
+        pos = atual->prox;
+    }
+    if(atual != NULL){
+        atual->prox = prev;
+        *Lista = atual;
+    }
+    else
+        *Lista = prev;
+}
+
+void Carrega_Baralho(LISTA_CARTAS_PTR *Baralho){
+    Deleta_Lista(Baralho);
+
+    FILE *Arquivo = fopen(LOCAL_FILE_BARALHO, "r");
+    if(Arquivo == NULL){
+        printf("Erro ao abrir o arquivo\n");
+        return;
+    }
+
+    int Naipe;
+    int Numero;
+    char Leitura;
+    while(!feof(Arquivo)){
+        Leitura = getc(Arquivo);
+        while(Leitura == '\n')
+            Leitura = getc(Arquivo);
+
+        Numero = Hexa_2_int(Leitura);
+
+        Leitura = getc(Arquivo);
+        while(Leitura == '\n')
+            Leitura = getc(Arquivo);
+
+        Naipe = Naipe_2_int(Leitura);
+
+        if (Leitura == -1)
+            break;
+
+        Cria_Carta(Baralho, Naipe, Numero);
+    }
+
+    Inverte_Lista(Baralho);
+}
+
+
+//-----------------------------------------------------------------------------------------------//
+void Imprime_Mesa_Organizado(LISTA_MESA_PTR *Mesa){
+
+}
+
+void Imprime_Mesa(LISTA_MESA_PTR *Mesa){
+    LISTA_MESA_PTR Atual = *Mesa;
     int cont = 0;
-    while(Baralho != NULL){
-        printf("%2d  -  %d | %d\n", cont, Baralho->numero, Baralho->naipe);
+    printf("--------------------------------------\n\n");
+    while(Atual != NULL){
+        printf("Mesa: %d.   X: %d, Y: %d\n", cont, Atual->x, Atual->y);
+        Imprime(Atual->cartas);
         cont++;
+        Atual = Atual->prox;
+    }
+}
+
+void Imprime(LISTA_CARTAS_PTR Baralho){
+    while(Baralho != NULL){
+        printf("%d | %d\n", Baralho->numero, Baralho->naipe);
         Baralho = Baralho->prox;
     }
     printf("\n");
@@ -120,122 +432,70 @@ void Imprime_Ordenado(LISTA_CARTAS_PTR Baralho){
 }
 
 
+LISTA_MESA_PTR duplicar_mesa (LISTA_MESA_PTR *lista_mesa){
 
-
-//verifica a validade da mesa (1-valido/0-invalido/-1 erro)
-int verifica_mesa(LISTA_MESA_PTR *lista_mesa){
+    LISTA_CARTAS_PTR nova_carta2 = NULL;
     LISTA_MESA_PTR atual_mesa = *lista_mesa;
-    //percorre os ponteiros da mesa até o ultimo
-    while(atual_mesa != NULL){
-        int qtd = 0;
-        LISTA_CARTAS_PTR monte1 = atual_mesa->cartas;
-        while(monte1 != NULL){
-            qtd++;
-            monte1 = monte1->prox;
-        }
-        //se tiver menos de 3 cartas é inválido
-        if (qtd < 3) return 0;
+    LISTA_MESA_PTR primeira_pos_mesa = NULL;
+    int i = 1;
 
-        monte1 = atual_mesa->cartas;   //reseta para a primera posição do monte
-        LISTA_CARTAS_PTR monte2 = atual_mesa->cartas;
-        monte2 = monte2->prox;
+    //se estiver vazio retorna NULL
+    if (*lista_mesa == NULL) return NULL;
 
-        //identifica o tipo de monte
-        //tipo 0: inválido
-        //tipo 1: sequencia de naipe igual
-        //tipo 2: grupo (cartas de numeros iguais de naipes diferentes)
 
-        //verificação de uso do naipe, utilizado no case 2
-        int usado[4]; //[1, 2, 3, 4] 1-true, 0-false
+    LISTA_MESA_PTR prev = NULL;
+    while (atual_mesa != NULL){
 
-        //seta todos para false
-        for (int i=0; i<4; i++){
-            usado[i] = 0;
+        LISTA_MESA_PTR nova_mesa = (LISTA_MESA_PTR)malloc(sizeof(LISTA_MESA));
+        nova_mesa->prox = NULL;
+        if (prev != NULL){
+            prev->prox = nova_mesa;
         }
 
+        //se for a primeira vez, armazena o ponteiro de inicio
 
-        int tipo = 0;
-        if (monte1->naipe == monte2->naipe && monte1->numero == ((monte2->numero) -1)) tipo = 1;
-        if (monte1->naipe != monte2->naipe && monte1->numero == monte2->numero) tipo = 2;
-        if (tipo == 0) return 0;
-        //marca como usados os naipes
-        usado[(monte1->naipe) -1] = 1;
-        usado[(monte2->naipe) -1] = 1;
-
-
-        //anda uma posição em cada
-        monte1 = monte1->prox;
-        monte2 = monte2->prox;
-
-
-        switch (tipo){
-        case 1:
-            while (monte2 != NULL){
-                if (monte1->naipe != monte2->naipe || monte1->numero != ((monte2->numero) -1)) return 0;
-                monte1 = monte1->prox;
-                monte2 = monte2->prox;
-            }
-        break;
-
-        case 2:
-            while (monte2 != NULL){
-                if (usado[(monte2->naipe) -1] == 1) return 0;
-                usado[(monte1->naipe) -1] = 1;
-                usado[(monte2->naipe) -1] = 1;
-                if (monte1->naipe == monte2->naipe || monte1->numero != monte2->numero) return 0;
-                monte1 = monte1->prox;
-                monte2 = monte2->prox;
-            }
-        break;
-
-        default:
-            return -1;
+        if (i == 1){
+            primeira_pos_mesa = nova_mesa;
         }
+
+        nova_mesa->N_Cartas = atual_mesa->N_Cartas;
+
+
+        nova_mesa->x = atual_mesa->x;
+
+        nova_mesa->y = atual_mesa->y;
+
+        LISTA_CARTAS_PTR atual_cartas = atual_mesa->cartas;
+        printf("atual_cartas->naipe = %d\n", atual_cartas->naipe);
+        atual_cartas->naipe;
+
+        LISTA_CARTAS_PTR nova_carta1 = (LISTA_CARTAS_PTR)malloc(sizeof(LISTA_CARTAS));
+
+        nova_carta1->naipe = atual_cartas->naipe;
+        printf("nova_carta1->naipe = %d\n", nova_carta1->naipe);
+        nova_carta1->numero = atual_cartas->numero;
+
+        nova_mesa->cartas = nova_carta1;
+        atual_cartas = atual_cartas->prox;
+        LISTA_CARTAS_PTR nova_carta2 = NULL;
+        while (atual_cartas != NULL){
+            printf("6\n");
+            nova_carta2 = (LISTA_CARTAS_PTR)malloc(sizeof(LISTA_CARTAS));
+            printf("a\n");
+            nova_carta2->naipe = atual_cartas->naipe;
+            printf("b\n");
+            nova_carta2->numero = atual_cartas->numero;
+            printf("c\n");
+            nova_carta1->prox = nova_carta2;
+            nova_carta1 = nova_carta2;
+            atual_cartas = atual_cartas->prox;
+        }
+        nova_carta2->prox = NULL;
+        i++;
+        printf("7\n");
         atual_mesa = atual_mesa->prox;
+        prev = nova_mesa;
     }
-    return 1;
-}
-
-
-//Tira a carta do baralho e coloca na mao do jogador, se
-void baralho_para_jogador (LISTA_CARTAS_PTR *baralho, LISTA_CARTAS_PTR *mao_jogador){
-    if (*baralho == NULL) return;
-
-    LISTA_CARTAS_PTR atual_baralho = *baralho;
-    LISTA_CARTAS_PTR atual_mao_jogador = *mao_jogador;
-
-    //andar ate a ultima posicao
-    while (atual_mao_jogador != NULL){
-        atual_mao_jogador = atual_mao_jogador->prox;
-    }
-
-    atual_mao_jogador->prox = *baralho;
-    *baralho = (*baralho)->prox;
-}
-
-
-//retira a carta da posição dada da lista, começando no 1
-void tirar_carta (LISTA_CARTAS_PTR *lista_cartas, int pos){
-    LISTA_CARTAS_PTR atual = *lista_cartas;
-    if (pos == 1){
-        *lista_cartas = *lista_cartas->prox;
-        return;
-    }
-
-    for (int i=0; i<pos-1; i++){
-        atual = atual->prox;
-    }
-    //Se não existir a posição não faz nada
-    if (atual == NULL) return;
-    LISTA_CARTAS_PTR temp = atual->prox;
-    atual->prox = temp->prox;
-}
-
-}
-//Tira a carta da posição 1 da mão do jogador e coloca na posição 2 do monte
-void jogador_para_monte (LISTA_CARTAS_PTR *mao_jogador, LISTA_CARTAS_PTR *monte, int pos1, int pos2){
-    LISTA_CARTAS_PTR lista_mao = *mao_jogador;
-    LISTA_CARTAS_PTR lista_monte = *monte;
-
-
+    printf("8\n");
+    return primeira_pos_mesa;
 }
